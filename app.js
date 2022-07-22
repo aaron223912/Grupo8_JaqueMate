95aeba7fa444cb8ac7d3d935b9500e1058d064fe
MMundanu <mmundanu@yahoo.com>
2022-07-22 19:17:33 -0300

    agrego imagenes y modifico app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = 3060;
 
 
-app.listen(3060, ()=>{
-    console.log("servidor corriendo en puerto 3060")
+app.listen(port, ()=>{
+    console.log("servidor corriendo en puerto http://localhost:" + port)
 });
 app.use(express.static("public"));
 app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname, "views", "home.html")));
