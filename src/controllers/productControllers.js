50f9e96a8feab5a344d475a07828b469b4f8f461
MMundanu <mmundanu@yahoo.com>
2022-08-16 20:08:21 -0300

    formularios

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -3,5 +3,11 @@
 module.exports = {
     detalle : (req, res) => {
         return res.render('detalleDeProducto')
+    },
+    editar : (req, res) => {
+        return res.render('editarProducto')
+    },
+    crear : (req, res) => {
+        return res.render('crearProducto')
     }
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {detalle} = require('../controllers/productControllers');
+const {detalle,editar,crear} = require('../controllers/productControllers');
 
 router
     .get('/detalle', detalle)
+    .get('/editarProducto', editar)
+    .get('/crearProducto', crear)
 
 module.exports = router;
