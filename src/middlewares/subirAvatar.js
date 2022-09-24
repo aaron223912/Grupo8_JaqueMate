420d269a66495ae452bb8a162146bdfebb9f5e33
bemja28 <bemja28091991@gmail.com>
2022-09-24 19:50:07 -0300

    multerFuncionando

--- src/middlewares/subirAvatar.js
+++ src/middlewares/subirAvatar.js
@@ -6,14 +6,13 @@ const storageUsers = multer.diskStorage({
     destination : (req, file, cb)=>{
         cb(null, "./public/images/users")
     },
-    filename : (req, file, cb) =>{
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-       // cb(null, "avatar" + Date.now()
+    filename:(req, file, cb)=>{
+        cb(null,'avatar' + Date.now() + path.extname(file.originalname))
 
     }
 })
 
-const uploatAvatar= multer({
-    storageUsers
-})
+const uploatAvatar = multer({
+    storage : storageUsers
+});
 module.exports = uploatAvatar
